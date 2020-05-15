3221030a739f42ff21a5eee1696d2268650bda46
chenfeng <924781530@qq.com>
2020-05-15 11:02:38 +0800

    firstcommit

---
+++ app/src/main/java/com/chenfengweather/android/db/Province.java
@@ -0,0 +1,32 @@
+package com.chenfengweather.android.db;
+
+public class Province {
+    private int id;
+    private String provinceName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    private int provinceCode;
+}
