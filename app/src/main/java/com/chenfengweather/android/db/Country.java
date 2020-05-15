1bf778689ec2a9125ff547f514b002b2714fcbf4
chenfeng <924781530@qq.com>
2020-05-15 13:30:25 +0800

    数据库添加

---
+++ app/src/main/java/com/chenfengweather/android/db/Country.java
@@ -0,0 +1,42 @@
+package com.chenfengweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Country extends LitePalSupport {
+    private int id;
+    private String CountryName;
+    private String weatherId;
+    private String cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return CountryName;
+    }
+
+    public void setCountryName(String countryName) {
+        CountryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public String getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(String cityId) {
+        this.cityId = cityId;
+    }
+}
